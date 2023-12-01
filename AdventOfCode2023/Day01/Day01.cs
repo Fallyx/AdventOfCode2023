d2e3f883915b8c085572e291ee72728c765d6b89
Fallyx <Fallyx@users.noreply.github.com>
2023-12-01 11:12:34 +0100

    Solved Day 01

---
+++ AdventOfCode2023/Day01/Day01.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode2023.Day01;
+
+internal class Day01
+{
+    const string inputPath = @"Day01/Input.txt";
+    private static readonly Dictionary<string, char> letters = new()
+    {
+        { "one", '1' }, { "1", '1' },
+        { "two", '2' }, { "2", '2' },
+        { "three", '3' }, { "3", '3' },
+        { "four", '4' }, { "4", '4' },
+        { "five", '5' }, { "5", '5' },
+        { "six", '6' }, { "6", '6' },
+        { "seven", '7' }, { "7", '7' },
+        { "eight", '8' }, { "8", '8' },
+        { "nine", '9' }, { "9", '9' }
+    };
+
+    internal static void Task1and2() 
+    {
+        List<String> lines = [.. File.ReadAllLines(inputPath)];
+
+        int calibValueTask1 = 0;
+        int calibValueTask2 = 0;
+
+        foreach (String line in lines)
+        {
+            char first = line.First(Char.IsDigit);
+            char last = line.Last(Char.IsDigit);
+
+            calibValueTask1 += int.Parse($"{first}{last}");
+            calibValueTask2 += StringToDigits(line);
+        }
+
+        Console.WriteLine($"Task 1: {calibValueTask1}");
+        Console.WriteLine($"Task 2: {calibValueTask2}");
+    }
+
+    private static int StringToDigits(string line)
+    {
+        int idxFirst = int.MaxValue;
+        int idxLast = int.MinValue;
+        char first = '0';
+        char last = '0';
+
+        foreach(KeyValuePair<String, char> keyValue in letters)
+        {
+            int idx1 = line.IndexOf(keyValue.Key);
+            int idx2 = line.LastIndexOf(keyValue.Key);
+
+            if (idx1 >= 0 && idx1 < idxFirst)
+            {
+                idxFirst = idx1;
+                first = keyValue.Value;
+            }
+
+            if (idx2 >= 0 && idx2 > idxLast)
+            {
+                idxLast = idx2;
+                last = keyValue.Value;
+            }
+        }
+
+        return int.Parse($"{first}{last}");
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2023.Day01;
+using System.Diagnostics;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        private static readonly Stopwatch stopwatch = new();
+
+        static void Main(string[] args)
+        {
+            string? input;
+            if (args.Length != 0) 
+            {
+                input = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Run a day [1-25]:");
+                input = Console.ReadLine();
+                Console.WriteLine("-----------------");
+            }
+
+            bool success = int.TryParse(input, out int day);
+            if (!success) return;
+
+            RunDay(day);
+        }
+
+        private static void RunDay(int day)
+        {
+            stopwatch.Start();
+
+            switch (day)
+            {
+                case 1:
+                    Day01.Day01.Task1and2();
+                    break;
+                default:
+                    break;
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Day {day:D2} finished in {stopwatch.Elapsed}");
+        }
+    }
+}
