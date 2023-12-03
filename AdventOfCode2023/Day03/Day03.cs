566662d36cd1ed52d5874f126138121f1a56c9bc
Fallyx <Fallyx@users.noreply.github.com>
2023-12-03 20:58:07 +0100

    Using old version and added task 2

--- AdventOfCode2023/Day03/Day03.cs
+++ AdventOfCode2023/Day03/Day03.cs
@@ -9,87 +9,88 @@ internal class Day03
     internal static void Task1and2()
     {
         List<String> lines = [.. File.ReadAllLines(inputPath)];
-        Dictionary<string, List<Vector2>> parts = [];
-        Dictionary<Vector2, char> symbols = [];
+        Dictionary<Vector2, List<int>> gears = [];
+        int partNumSum = 0;
 
         for (int y = 0; y < lines.Count; y++)
         {
             for (int x = 0; x < lines[y].Length; x++)
             {
-                if (lines[y][x] == '.') continue;
+                if (!Char.IsDigit(lines[y][x])) continue;
 
-                if (!Char.IsDigit(lines[y][x]))
+                string number = $"{lines[y][x]}";
+                int numLength = 0;
+                bool numEnded = false;
+
+                while (!numEnded)
                 {
-                    symbols.Add(new Vector2(x, y), lines[y][x]);
-                } 
-                else
+                    numLength++;
+                    if (x + numLength < lines[y].Length && Char.IsDigit(lines[y][x + numLength]))
+                        number += lines[y][x + numLength];
+                    else
+                        numEnded = true;
+                }
+
+                int subStringStart = x - 1 >= 0 ? x - 1 : 0;
+                int subStringEnd = (x + numLength + 1 < lines[y].Length ? x + numLength + 1 : x + numLength) - subStringStart;
+                string lineTop = (y - 1 >= 0 ? lines[y - 1].Substring(subStringStart, subStringEnd) : string.Empty);
+                string lineMiddle = lines[y].Substring(subStringStart, subStringEnd);
+                string lineBottom = (y + 1 < lines.Count ? lines[y + 1].Substring(subStringStart, subStringEnd) : string.Empty);
+
+                (bool possible, char symbol, int xRelative, int yRelative) parsedNumber = IsPartNumber(lineTop, lineMiddle, lineBottom);
+
+                if (parsedNumber.possible)
                 {
-                    string number = "" + lines[y][x];
-
-                    int numLength = 0;
-                    bool numEnded = false;
-
-                    while (!numEnded)
+                    int numberInt = int.Parse(number);
+                    partNumSum += numberInt;
+                    if (parsedNumber.symbol == '*')
                     {
-                        numLength++;
-                        if (x + numLength < lines[y].Length && Char.IsDigit(lines[y][x + numLength]))
-                            number = number + lines[y][x + numLength];
+                        if (x - 1 < 0) parsedNumber.xRelative++;
+                        Vector2 gearPos = new Vector2(x + parsedNumber.xRelative, y + parsedNumber.yRelative);
+                        if (gears.ContainsKey(gearPos))
+                            gears[gearPos].Add(numberInt);
                         else
-                            numEnded = true;
+                            gears.Add(gearPos, [numberInt]);
                     }
-
-                    List<Vector2> adjacents = [];
-
-                    for (int adjY = y - 1; adjY <= y + 1; adjY++)
-                    {
-                        for (int adjX = x - 1; adjX <= x + numLength; adjX++)
-                        {
-                            if (adjY == y && adjX >= x && adjX <= x + numLength - 1) continue;
-
-                            adjacents.Add(new Vector2(adjX, adjY));
-                        }
-                    }
-
-                    parts.Add($"{number}-{x}:{y}", adjacents);
-                    x += numLength - 1;
                 }
-            }
-        }
 
-        int partNumSum = 0;
-
-        foreach (KeyValuePair<string, List<Vector2>> part in parts)
-        {
-            foreach(KeyValuePair<Vector2, char> symbol in symbols)
-            {
-                if (part.Value.Contains(symbol.Key))
-                {
-                    partNumSum += int.Parse(part.Key.Split('-')[0]);
-                    break;
-                }
+                x += numLength;
             }
         }
 
         Console.WriteLine($"Task 1: {partNumSum}");
 
-        int gearSum = 0;
+        int gearScore = 0;
 
-        foreach (KeyValuePair<Vector2, char> symbol in symbols)
+        foreach (KeyValuePair<Vector2, List<int>> gear in gears)
         {
-            int gearRatio = 1;
-            int gearAdj = 0;
-            foreach (KeyValuePair<string, List<Vector2>> part in parts)
-            {
-                if (part.Value.Contains(symbol.Key))
-                {
-                    gearAdj++;
-                    gearRatio *= int.Parse(part.Key.Split('-')[0]);
-                }
-            }
-
-            if (gearAdj == 2) gearSum += gearRatio;
+            if (gear.Value.Count == 2)
+                gearScore += gear.Value.Aggregate(1, (a, b) => a * b);
         }
 
-        Console.WriteLine($"Task 2: {gearSum}");
+        Console.WriteLine($"Task 2: {gearScore}");
+    }
+
+    private static (bool possible, char symbol, int xRelative, int yRelative) IsPartNumber(string lineTop, string lineMiddle, string lineBototm)
+    {
+        for (int x = 0; x < lineTop.Length; x++)
+        {
+            if (lineTop[x] != '.' && !Char.IsDigit(lineTop[x])) 
+                return (true, lineTop[x], x - 1, -1);
+        }
+
+        for (int x = 0; x < lineMiddle.Length; x++)
+        {
+            if (lineMiddle[x] != '.' && !Char.IsDigit(lineMiddle[x])) 
+                return (true, lineMiddle[x], x - 1, 0);
+        }
+
+        for (int x = 0; x < lineBototm.Length; x++)
+        {
+            if (lineBototm[x] != '.' && !Char.IsDigit(lineBototm[x]))
+                return (true, lineBototm[x], x - 1, 1);
+        }
+
+        return (false, '.', int.MinValue, int.MinValue);
     }
 }
